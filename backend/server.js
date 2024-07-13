12ae4c60856eafa400d61abab9d02857943f6556
Yugal Kaushik <yugalkaushik14@gmail.com>
2024-07-13 22:37:27 +0530

    update backend server

--- backend/server.js
+++ backend/server.js
@@ -12,9 +12,6 @@ connectDB();
 app.use(cors());
 app.use(express.json());
 
-app.use('/images', express.static(path.join(__dirname, 'images')));
-
-
 app.use('/api/auth', require('./routes/authRoutes'));
 app.use('/api/products', require('./routes/productRoutes'));
 app.use('/api/navlinks', require('./routes/navLinkRoutes'));
