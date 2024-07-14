6c37f929291c3628106a4b095615f00c83493ba3
Yugal Kaushik <yugalkaushik14@gmail.com>
2024-07-14 16:28:03 +0530

    fixed errors

--- frontend/src/assets/icons/index.js
+++ frontend/src/assets/icons/index.js
@@ -1,7 +1,4 @@
-import facebook from './facebook.svg'
 import hamburger from './hamburger.svg'
-import instagram from './instagram.svg'
-import twitter from './twitter.svg'
 import search from './search.png'
 import wishlist from './heart.png'
 import cart from './shopping-bag.png'
@@ -9,10 +6,7 @@ import navLogo from './navLogo.png'
 
 const icons = {
     navLogo,
-    facebook,
     hamburger,
-    instagram,
-    twitter,
     search,
     cart,
     wishlist
