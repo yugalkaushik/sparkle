ef300ddfc462c1566d29da6f3b8c52ffffe11272
Yugal Kaushik <yugalkaushik14@gmail.com>
2024-07-20 18:25:32 +0530

    update models

--- backend/models/User.js
+++ backend/models/User.js
@@ -4,9 +4,10 @@ const userSchema = new mongoose.Schema({
   name: { type: String, required: true },
   email: { type: String, required: true, unique: true },
   password: { type: String, required: true },
-  age: { type: Number, required: true },
   gender: { type: String, required: true },
-  isVerified: { type: Boolean, default: false }
+  isVerified: { type: Boolean, default: false },
+  cart: [{type: mongoose.Schema.Types.ObjectId, ref:'Product'}],
+  wishlist: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Product' }]
 });
 
 module.exports = mongoose.model('User', userSchema);
