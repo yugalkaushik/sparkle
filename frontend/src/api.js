c5b422eb74d5c09cac319ea76f8ad2ec9d98a5bc
Yugal Kaushik <yugalkaushik14@gmail.com>
2024-07-26 15:30:16 +0530

    update api

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL:'https://sparkle-fxjd.onrender.com/api';
+    baseURL:'https://sparkle-fxjd.onrender.com/api'
 });
 
 export default api;
